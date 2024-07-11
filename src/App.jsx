918eec61dcc975daad775f1aa7227c15fcdc6f2b
Wesley Gado <wesleygado@gmail.com>
2024-07-11 14:54:10 -0300

    Aula 04 | 11 - Efeitos - useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function Contador({ numero, ...props }) {
   return (
     <>
       <span>{numero}</span><br />
-      <button {...props}>Somar</button>
+      <button {...props}>Somar</button><br />
+    </>
+  )
+}
+
+function Contador2({ numero, ...props }) {
+  return (
+    <>
+      <span>{numero}</span><br />
+      <button {...props}>Somar 2</button>
     </>
   )
 }
 
 function App() {
   const [contador, setContador] = useState(0);
+  const [contador2, setContador2] = useState(0);
 
-
-  function handleClick(numero) {
-    setContador(contador + numero);
-  }
+  useEffect(() => {
+    console.log('Contador2 atualizado');
+  }, [contador2]);
 
   return (
-    <Contador
-      numero={contador}
-      onClick={() => {handleClick(2)}} />
+    <>
+      <Contador
+        numero={contador}
+        onClick={() => setContador(contador + 1)} />
+
+      <Contador2
+        numero={contador2}
+        onClick={() => setContador2(contador2 + 1)} />
+    </>
   )
 }
 
