6b60ef655324938340e258674f0ebd3e8b903056
Wesley Gado <wesleygado@gmail.com>
2024-07-11 15:53:17 -0300

    Aula 04 | 12 - Ciclo de vida dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,28 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function Contador({ numero, ...props }) {
-  return (
-    <>
-      <span>{numero}</span><br />
-      <button {...props}>Somar</button><br />
-    </>
-  )
-}
 
-function Contador2({ numero, ...props }) {
+  useEffect(() => {
+    console.log('Componente Criado');
+    return () => {
+      console.log('Componente Removido');
+    }
+  });
+
   return (
     <>
       <span>{numero}</span><br />
-      <button {...props}>Somar 2</button>
     </>
   )
 }
 
 function App() {
   const [contador, setContador] = useState(0);
-  const [contador2, setContador2] = useState(0);
-
-  useEffect(() => {
-    console.log('Contador2 atualizado');
-  }, [contador2]);
 
   return (
     <>
-      <Contador
-        numero={contador}
-        onClick={() => setContador(contador + 1)} />
-
-      <Contador2
-        numero={contador2}
-        onClick={() => setContador2(contador2 + 1)} />
+      <button onClick={() => setContador(contador + 1)}>Somar</button><br />
+      {contador % 2 === 0 && <Contador numero={contador} />}
     </>
   )
 }
