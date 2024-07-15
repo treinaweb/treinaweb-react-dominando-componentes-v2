3197fe486067e13d308b30a45010fab3d98dfc04
Wesley Gado <wesleygado@gmail.com>
2024-07-15 18:32:43 -0300

    Aula 05 | 17 - Fragmentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  const minhaLista = [
-    { id: 2, nome: 'João', idade: 23 },
-    { id: 1, nome: 'Maria', idade: 30 },
-    { id: 3, nome: 'Bruno', idade: 32 },
-    { id: 4, nome: 'Marcela', idade: 45 },
-  ];
+function Lista() {
+  return (
+    <>
+      <li>Dados da minha lista</li>
+      <li>Dados da minha lista</li>
+    </>
+  )
+}
 
-  function exibirNome(nome) {
-    console.log(nome);
-  }
+function App() {
 
   return (
     <div>
       <ul>
-        {minhaLista.map((pessoa, index) => (
-          <li key={index} onClick={() => exibirNome(pessoa.nome)}>
-            {`Nome: ${pessoa.nome} - Idade: ${pessoa.idade}`}
-          </li>
+        {[1, 2, 3].map((numero, index) => (
+          <React.Fragment key={index}>
+            <li>Elemnto 1</li>
+            <li>Elemnto 2</li>
+            <li>Elemnto 3</li>
+          </React.Fragment>
         ))}
       </ul>
     </div>
