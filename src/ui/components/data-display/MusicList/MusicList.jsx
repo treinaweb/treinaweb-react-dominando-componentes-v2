aaade4437083bf8faf359f2a416ce5666773cc54
Wesley Gado <wesleygado@gmail.com>
2024-07-17 01:03:18 -0300

    Projeto - Componente Lista de músicas 2

--- src/ui/components/data-display/MusicList/MusicList.jsx
+++ src/ui/components/data-display/MusicList/MusicList.jsx
@@ -1,29 +1,25 @@
+import { TimeService } from '../../../../data/services/TimeService';
 import styles from './MusicList.module.css';
 
-export default function MusicList() {
+export default function MusicList(props) {
+  function isSelected(music) {
+    return props.selectedMusic === music ? styles.selected : '';
+  }
+
   return (
     <ul className={styles.listMusic}>
-      <li className={styles.musicListItem}>
-        <div className={styles.information}>
-          <span className={styles.title}>Titulo Música</span>
-          <span className={styles.artist}>Artista</span>
-          <span className={styles.time}>02:40</span>
-        </div>
-      </li>
-      <li className={styles.musicListItem}>
-        <div className={styles.information}>
-          <span className={styles.title}>Titulo Música</span>
-          <span className={styles.artist}>Artista</span>
-          <span className={styles.time}>02:40</span>
-        </div>
-      </li>
-      <li className={styles.musicListItem}>
-        <div className={styles.information}>
-          <span className={styles.title}>Titulo Música</span>
-          <span className={styles.artist}>Artista</span>
-          <span className={styles.time}>02:40</span>
-        </div>
-      </li>
+      {props.musics.map((music) => (
+         <li className={[
+                styles.musicListItem,
+                isSelected(music)].join(' ')}
+                onClick={() => props.OnSelect(music)}>
+         <div className={styles.information}>
+           <span className={styles.title}>{music.name}</span>
+           <span className={styles.artist}>{music.artist}</span>
+           <span className={styles.time}>{TimeService.timeDisplay(music.time)}</span>
+         </div>
+       </li>
+      ))}
     </ul>
   )
 }
--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,5 +1,27 @@
 import MusicList from "../components/data-display/MusicList/MusicList";
 
+const musics = [
+  {
+    id: 1,
+    name: 'Abc',
+    artist: '123',
+    time: 95
+  },
+  {
+    id: 2,
+    name: 'def',
+    artist: '456',
+    time: 189
+  }
+]
+
 export default function Index() {
-  return <MusicList />
+  return (
+    <div>
+      <MusicList 
+        musics={musics}
+        selectedMusic={musics[0]} 
+        OnSelect={() => {}}/>
+    </div>
+  )
 }
