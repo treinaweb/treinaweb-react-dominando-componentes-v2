8ac69a70b8e6edc3e9ff1c2738894e246e9d7f63
Wesley Gado <wesleygado@gmail.com>
2024-07-17 23:44:08 -0300

    Projeto - Lógica da aplicação

--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,34 +1,27 @@
 import MusicList from "../components/data-display/MusicList/MusicList";
 import AudioPlayer from "../components/data-display/AudioPlayer/AudioPlayer";
 import styles from "./index.module.css";
-
-const musics = [
-  {
-    id: 1,
-    name: 'Abc',
-    artist: '123',
-    time: 95,
-    url: 'musics/Shadowing.mp3'
-  },
-  {
-    id: 2,
-    name: 'def',
-    artist: '456',
-    time: 189,
-    url: 'musics/Stoker.mp3'
-  }
-]
+import { useContext } from "react";
+import { AppContext } from "../../App";
 
 export default function Index() {
+  const { 
+    selectedMusic, 
+    time, 
+    setTime, 
+    selectMusic, 
+    musicList 
+  } = useContext(AppContext);
+
   return (
     <div className={styles.pageContainer}>
-      <MusicList 
-        musics={musics}
-        selectedMusic={musics[0]} 
-        OnSelect={() => {}}/>
-      <AudioPlayer 
-        music={musics[0]} 
-        onComplete={() => console.log('Finish')}/>
+      <MusicList
+        musics={musicList}
+        selectedMusic={selectedMusic}
+        OnSelect={selectMusic} />
+      <AudioPlayer
+        music={selectedMusic}
+        onComplete={() => console.log('Finish')} />
     </div>
   )
 }
